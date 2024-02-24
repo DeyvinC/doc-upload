79806f726f3661b90b4cf6cea099c0937525c746
“Deyvin <deyvinvelasquez0202@gmail.com>
2024-02-23 21:18:58 -0500

    added new logic to web

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FaSquareXmark } from "react-icons/fa6";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='mainCon'>
+        <div className='mainTitleCon'>
+          <div className='iconCon'>
+            <FaSquareXmark color='#0b125c' size={25} />
+          </div>
+          <div className='headerCon'>
+            <p className='headerText'>Document Upload</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
